3f83ba1cf48fd3ad7847aa8fc0574dc9f4154a55
LENOIR Remi <remi.lenoir@infotel.com>
2024-11-06 15:09:30 +0100

    add support to new microsoft teams

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # teams-gifs
 
-Add gif as webcam background on Microsoft Teams.
+Add gifs as webcam background on Microsoft Teams.
 
 > This application is meant to be run using Windows CMD.
 
 1. Place gifs in the gifs folder.
-2. Run `node index.js`. (Teams will restart.)
+2. Install dependencies `npm install`.
+3. Run `node index.js`. (new Microsoft Teams 2024 version) (Teams will restart.) 
+
+Or
+
+3. Run `node index.js --classic` (Microsoft Teams classic version) (Teams will restart.)
--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // Import dependencies
 const fs = require('fs');
 const os = require('os');
+const { v4: uuidv4 } = require('uuid');
 const { exec } = require('child_process');
 
-// relative path to the folder containing gifs
+// Argument for using the Microsoft Teams classic flow
+const classicTeams = process.argv.includes('--classic');
+
+// Relative path to the folder containing gifs
 const GIFS_DIR = 'gifs';
 
-// Window username
+// Windows username
 const windowsUser = os.userInfo().username;
 
 // List gifs
@@ -14,13 +18,36 @@ const dirCont = fs.readdirSync(GIFS_DIR);
 const gifs = dirCont.filter(f => f.match(/.*\.(gif?)/ig));
 
 // Migrate gifs to Backgrounds with _thumb files.
-gifs.forEach(g => {
-    let renameFilename = g.replace('.gif', '.png');
-    let renameFilenameThumb = g.replace('.gif', '_thumb.png');
-    fs.copyFileSync(`${GIFS_DIR}/${g}`, `C:/Users/${windowsUser}/AppData/Roaming/Microsoft/Teams/Backgrounds/Uploads/${renameFilename}`);
-    fs.copyFileSync(`${GIFS_DIR}/${g}`, `C:/Users/${windowsUser}/AppData/Roaming/Microsoft/Teams/Backgrounds/Uploads/${renameFilenameThumb}`);
-    fs.unlinkSync(`${GIFS_DIR}/${g}`);
-});
+if (classicTeams) {
+    // Microsoft Teams classic
+    gifs.forEach(g => {
+        const renameFilename = g.replace('.gif', '.png');
+        const renameFilenameThumb = g.replace('.gif', '_thumb.png');
+        try {
+            fs.copyFileSync(`${GIFS_DIR}/${g}`, `C:/Users/${windowsUser}/AppData/Roaming/Microsoft/Teams/Backgrounds/Uploads/${renameFilename}`);
+            fs.copyFileSync(`${GIFS_DIR}/${g}`, `C:/Users/${windowsUser}/AppData/Roaming/Microsoft/Teams/Backgrounds/Uploads/${renameFilenameThumb}`);
+            fs.unlinkSync(`${GIFS_DIR}/${g}`);
+        } catch (error) { }
+    });
+} else {
+    // Microsoft Teams new (2024)
+    gifs.forEach(g => {
+        const uuid = uuidv4();
+        const renameFilename = `${uuid}.png`;
+        const renameFilenameThumb = `${uuid}_thumb.png`;
+        try {
+            fs.copyFileSync(`${GIFS_DIR}/${g}`, `C:/Users/${windowsUser}/AppData/Local/Packages/MSTeams_8wekyb3d8bbwe/LocalCache/Microsoft/MSTeams/Backgrounds/Uploads/${renameFilename}`);
+            fs.copyFileSync(`${GIFS_DIR}/${g}`, `C:/Users/${windowsUser}/AppData/Local/Packages/MSTeams_8wekyb3d8bbwe/LocalCache/Microsoft/MSTeams/Backgrounds/Uploads/${renameFilenameThumb}`);
+            fs.unlinkSync(`${GIFS_DIR}/${g}`);
+        } catch (error) { }
+    });
+}
 
 // Restart Teams (Kill and Restart)
-exec(`Taskkill /IM Teams.exe /F; Start-Process -File $env:LOCALAPPDATA\\Microsoft\\Teams\\Update.exe -ArgumentList '--processStart "Teams.exe"'`, { 'shell': 'powershell.exe' });
+if (classicTeams) {
+    // Microsoft Teams classic
+    exec(`Taskkill /IM Teams.exe /F; Start-Process -File $env:LOCALAPPDATA\\Microsoft\\Teams\\Update.exe -ArgumentList '--processStart "Teams.exe"'`, { shell: 'powershell.exe' });
+} else {
+    // Microsoft Teams new (2024)
+    exec(`Taskkill /IM ms-teams.exe /F; Start-Process ms-teams.exe`, { shell: 'powershell.exe' });
+}
